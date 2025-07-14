b6e965b00450236449e9e2356aece2c23fb2aa1e
Math012 <math111.0811@gmail.com>
2025-07-14 09:12:07 -0300

    primeiro commit

---
+++ src/main/java/com/math012/apimensagenssms/ApiMensagensSmsApplication.java
@@ -0,0 +1,13 @@
+package com.math012.apimensagenssms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiMensagensSmsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiMensagensSmsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/math012/apimensagenssms/ApiMensagensSmsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.math012.apimensagenssms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiMensagensSmsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
